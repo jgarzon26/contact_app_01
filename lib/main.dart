bc3619c458aeb3b415356850171c57884049ae93
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-23 13:18:09 +0800

    SettingUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,16 @@
+import 'package:contact_app_01/screens/contact_list/contact_list.dart';
 import 'package:flutter/material.dart';
 
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      routes: {
+        '/' : (context) => ContactList(),
+      },
+    );
+  }
+}
