a14eeb485673ccd9b0d114a8684cdda2cf83b838
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-24 11:20:46 +0800

    adding uuid to contact class

--- lib/classes/contact.dart
+++ lib/classes/contact.dart
@@ -1,6 +1,8 @@
+import 'package:uuid/uuid.dart';
 
 class Contact{
+  final String id = const Uuid().v4();
   final String name;
 
-  const Contact({required this.name});
+  Contact({required this.name});
 }
