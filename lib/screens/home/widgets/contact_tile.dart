edea55ad0329e7f1af00f41de160ddc3f73240d0
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-24 12:10:13 +0800

    Adding dismissable and remove data from index

--- lib/screens/home/widgets/contact_tile.dart
+++ lib/screens/home/widgets/contact_tile.dart
@@ -1,3 +1,4 @@
+import 'package:contact_app_01/classes/contact_book.dart';
 import 'package:flutter/material.dart';
 
 import '../../../classes/contact.dart';
@@ -10,14 +11,24 @@ class ContactTile extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return ListTile(
-      leading: const CircleAvatar(
-        child: Icon(
-          Icons.person,
+    return Dismissible(
+      onDismissed: (direction) {
+        ContactBook().removeContact(contact);
+      },
+      key: ValueKey(contact.id),
+      child: Material(
+        color: Colors.white,
+        elevation: 6.0,
+        child: ListTile(
+          leading: const CircleAvatar(
+            child: Icon(
+              Icons.person,
+            ),
+          ),
+          title: Text(
+            contact.name,
+          ),
         ),
-      ),
-      title: Text(
-        contact.name,
       ),
     );
   }
