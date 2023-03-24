a1901a994d07f61e6f3f5b6e37cdedd1ae549676
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-24 10:34:29 +0800

    Adding basic theme and navigation to new contact

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:contact_app_01/screens/home/home.dart';
+import 'package:contact_app_01/screens/new_contact/new_contact.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
@@ -8,8 +9,19 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.white,
+            letterSpacing: 4,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       routes: {
         '/' : (context) => Home(),
+        '/new' : (context) => NewContact(),
       },
     );
   }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,5 @@
 import 'package:contact_app_01/screens/home/widgets/contact_tile.dart';
 import 'package:flutter/material.dart';
-import '../../classes/contact.dart';
 import '../../classes/contact_book.dart';
 
 class Home extends StatelessWidget{
@@ -15,8 +14,9 @@ class Home extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text(
-          'Contact',
+        title: Text(
+          'contact'.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
       body: CustomScrollView(
@@ -30,6 +30,12 @@ class Home extends StatelessWidget{
             ),
           )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).pushNamed('/new'),
+        child: const Icon(
+          Icons.person_add_alt_1,
+        ),
       ),
     );
   }
